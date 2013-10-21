e849857160f30e80fe0d5623333d438a77043c92
Neil Green <neil@melessoftware.com>
2013-10-21 19:56:36 +0100

    extracted junit Threads rule from stm

---
+++ src/main/java/com/melessoftware/testing/concurrent/TestThread.java
@@ -0,0 +1,71 @@
+/*
+ * Meles Concurrent Testing - a library for testing concurrency
+ * Copyright (C) 2013 Neil Green
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.melessoftware.testing.concurrent;
+
+import static java.util.concurrent.Executors.callable;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+public class TestThread {
+
+    private final ExecutorService executorService = createExecutorService();
+
+    public void proceed(Runnable runnable) throws Throwable {
+        proceed(callable(runnable));
+    }
+
+    public <X> X proceed(Callable<X> callable) throws Throwable {
+        final Future<X> futureX = executorService.submit(callable);
+        final X x;
+        try {
+            x = futureX.get();
+        } catch (ExecutionException e) {
+            throw e.getCause();
+        }
+        return x;
+    }
+
+    void close() {
+        executorService.shutdownNow();
+    }
+
+    void awaitClosure(long timeout, TimeUnit timeUnit) throws InterruptedException {
+        executorService.awaitTermination(timeout, timeUnit);
+    }
+
+    private static ExecutorService createExecutorService() {
+        return Executors.newSingleThreadExecutor(new ThreadFactory() {
+            private volatile boolean threadCreated = false;
+
+            @Override
+            public Thread newThread(Runnable r) {
+                if (threadCreated) {
+                    // ensure we only run on one thread
+                    throw new RuntimeException("attempt to create a second thread in TestThread");
+                }
+                final Thread thread = Executors.defaultThreadFactory().newThread(r);
+                threadCreated = true;
+                return thread;
+            }
+        });
+    }
+}
---
+++ src/main/java/com/melessoftware/testing/concurrent/Threads.java
@@ -0,0 +1,69 @@
+/*
+ * Meles Concurrent Testing - a library for testing concurrency
+ * Copyright (C) 2013 Neil Green
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.melessoftware.testing.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class Threads implements TestRule {
+
+    private List<TestThread> threads;
+
+    private static final long DEFAULT_TIMEOUT_NANOS = TimeUnit.SECONDS.toNanos(2);
+
+    private final long timeoutNanos = DEFAULT_TIMEOUT_NANOS;
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                threads = new ArrayList<>();
+                try {
+                    base.evaluate();
+                } finally {
+                    shutdownThreads();
+                }
+            }
+        };
+    }
+
+    public TestThread create() {
+        final TestThread newThread = new TestThread();
+        threads.add(newThread);
+        return newThread;
+    }
+
+    private void shutdownThreads() throws InterruptedException {
+        for (TestThread thread : threads) {
+            thread.close();
+        }
+        final long waitStart = System.nanoTime();
+        long waitRemainingNanos = timeoutNanos;
+        for (TestThread thread : threads) {
+            if (waitRemainingNanos <= 0) {
+                break;
+            }
+            thread.awaitClosure(waitRemainingNanos, TimeUnit.NANOSECONDS);
+            waitRemainingNanos = timeoutNanos - (System.nanoTime() - waitStart);
+        }
+    }
+}
